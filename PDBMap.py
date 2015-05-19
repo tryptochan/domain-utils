023dbcfff021056554dad267c18de2d3808518df
Yuxing Liao <yuxingliao@gmail.com>
2015-05-19 17:21:00 -0500

    Bug fixes. PDB chain ID should be parsed from pdb_strand_id

--- PDBMap.py
+++ PDBMap.py
@@ -29,21 +29,25 @@ class PDBMap(object):
 
     def _start_handler(self, name, attrs):
         if name == 'PDBx:pdbx_poly_seq_scheme':
-            self._chain = attrs['asym_id']
+            # also serves as the flag in poly_seq
             self._seq_idx = attrs['seq_id']
-            if self._chain not in self.mapping:
-                self.mapping[self._chain] = {'seq': [], 'pdb': []}
-        elif self._chain and name == 'PDBx:auth_seq_num':
+        elif self._seq_idx and name == 'PDBx:pdb_strand_id':
+            self._tag = 'pdb_strand_id'
+        elif self._seq_idx and name == 'PDBx:auth_seq_num':
             self._isordered = True
             self._tag = 'auth_seq_num'
-        elif self._chain and name == 'PDBx:pdb_ins_code':
+        elif self._seq_idx and name == 'PDBx:pdb_ins_code':
             self._tag = 'pdb_ins_code'
 
     def _data_handler(self, data):
-        if self._chain and self._tag == 'auth_seq_num':
+        if self._seq_idx and self._tag == 'auth_seq_num':
             self._pdb_idx = data
-        elif self._chain and self._tag == 'PDBx:pdb_ins_code':
+        elif self._seq_idx and self._tag == 'pdb_ins_code':
             self._inscode = data
+        elif self._seq_idx and self._tag == 'pdb_strand_id':
+            self._chain = data
+            if self._chain not in self.mapping:
+                self.mapping[self._chain] = {'seq': [], 'pdb': []}
 
     def _end_handler(self, name):
         if name == 'PDBx:pdbx_poly_seq_scheme':
